154009d768522f29c3e79e2c20c8f7747e8d2282
Jagan-Mathematics <jaganmathematics@gmail.com>
2021-05-10 09:47:58 +0530

    script flake test corrections basic folder

--- basics/apache beam/basic_pipeline.py
+++ basics/apache beam/basic_pipeline.py
@@ -3,26 +3,27 @@ import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import SetupOptions
+
+
 input_file = "gs://dataflow-samples/shakespeare/kinglear.txt"
 output_file = "output.txt"
 # Define pipeline options object.
-pipeline_options = PipelineOptions(DEFAULT_RUNNER= 'DirectRunner')
+pipeline_options = PipelineOptions(DEFAULT_RUNNER='DirectRunner')
 with beam.Pipeline(options=pipeline_options) as p:
-	# Read the text file or file pattern into a PCollection.
-	lines = p | ReadFromText(input_file)
-	# Count the occurrences of each word.
-	counts = (
-	lines
-	| 'Split' >> beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
-	| 'PairWithOne' >> beam.Map(lambda x: (x, 1))
-	| 'GroupAndSum' >> beam.CombinePerKey(sum))
-	
-	# Format the counts into a PCollection of strings.
-	def format_result(word_count):
-		(word, count) = word_count
-		return "{}: {}".format(word, count)
-	
-	output = counts | 'Format' >> beam.Map(format_result)
-	# Write the output using a "Write" transform that has side effects.
-	output | WriteToText(output_file)
+    # Read the text file or file pattern into a PCollection.
+    lines = p | ReadFromText(input_file)
+    # Count the occurrences of each word.
+    counts = (
+        lines
+        | 'Split' >> beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
+        | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+        | 'GroupAndSum' >> beam.CombinePerKey(sum))
+
+    # Format the counts into a PCollection of strings.
+    def format_result(word_count):
+        (word, count) = word_count
+        return "{}: {}".format(word, count)
+
+    output = counts | 'Format' >> beam.Map(format_result)
+    # Write the output using a "Write" transform that has side effects.
+    output | WriteToText(output_file)
